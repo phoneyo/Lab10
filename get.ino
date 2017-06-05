223d80c73d4d0daea6c7a6022fff84eccf5cc86e
phoneyo <asdzxc4563@gmail.com>
2017-06-05 20:01:56 +0800

    Add files via upload

---
+++ get.ino
@@ -0,0 +1,28 @@
+#include <IRremote.h>
+
+// 　　接收紅外線
+
+const int irReceiverPin = 2;
+
+IRrecv irrecv(irReceiverPin);
+decode_results results;
+void setup() 
+{
+  Serial.begin(9600);
+  irrecv.enableIRIn();
+}
+
+void loop()
+{
+ if(irrecv.decode(&results))
+ {
+  Serial.print("irCode : ");
+  Serial.println(results.value, HEX);
+  Serial.print(", bits : ");
+  Serial.println(results.bits);
+  irrecv.resume();
+ } 
+  
+  
+  
+}
---
+++ send.ino
@@ -0,0 +1,36 @@
+#include<IRremote.h>
+const int irReceiverPin = 2;
+const int buttonPin = 4;
+int buttonState = 0;
+IRsend irsend;
+IRrecv irrecv(irReceiverPin);
+decode_results results;
+void setup() 
+{
+  Serial.begin(9600);
+  /*Serial.begin(9600);
+  irrecv.enableIRIn();
+  Serial.println("04050343  04050794  START");*/
+  pinMode(buttonPin, INPUT);
+}
+
+void loop() 
+{
+ /* if(irrecv.decode(&results))
+  {
+    Serial.print("irCode: ");
+    Serial.print(results.value, HEX);
+    Serial.print(" ,  bit: ");
+    Serial.println(results.bits);
+    irrecv.resume();
+    }*/
+   buttonState = digitalRead(buttonPin);
+   
+   if(buttonState == HIGH)
+   {
+    irsend.sendNEC(0x4FB48B7, 32);
+    Serial.print("X");
+    
+    }
+   
+}
